6292d1eda6f7085b55b3288e4121449f2f5b54b2
Jon Mockbee <mockbee1000@gmail.com>
2021-08-22 13:21:40 -0400

    first exercise

---
+++ example1.py
@@ -0,0 +1,8 @@
+def double_word(word):
+    word = word * 2
+    return word + str(len(word))
+
+
+print(double_word("hello"))  # Should return hellohello10
+print(double_word("abc"))  # Should return abcabc6
+print(double_word(""))  # Should return 0
