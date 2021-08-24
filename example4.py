d40ed8980c2f4ddc545a5fa3b1329df57bd30400
Jon Mockbee <mockbee1000@gmail.com>
2021-08-23 21:23:11 -0400

    figuring out how the string does not change just adds new message

--- example4.py
+++ example4.py
@@ -2,3 +2,9 @@ message = "A kong silly string with a typo"
 
 new_message = message[0:2] + "l" + message[3:]
 print(new_message)
+
+message = " this is a new message "
+print(message)
+
+message = "and another one "
+print(message)
